d65afb95b7ca3ee8731c7c0f2b760eddc93ba3ea
DanielK <gnorimajima@gmail.com>
2024-11-04 16:59:07 -0400

    adicionado exemplo de interpolacao de strings

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+//Concatenacao
 string nome = "Daniel";
 string saudacao = "Ola, " + nome + "! Bem-vindo";
 Console.WriteLine(saudacao);
+//Interpolacao de strings
+string nome1 = "Daniel";
+string saudacao1 = $"Ola, {nome}! Bem-vindo."
+Console.WriteLine(saudacao1)
