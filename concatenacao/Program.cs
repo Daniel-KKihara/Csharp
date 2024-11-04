c468d7e2d8bb69e0920104920aa9a6510f234d50
DanielK <gnorimajima@gmail.com>
2024-11-04 17:08:11 -0400

    adicionado exemplo de GetType()

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -5,5 +5,9 @@ string saudacao = "Ola, " + nome + "! Bem-vindo";
 Console.WriteLine(saudacao);
 //Interpolacao de strings
 string nome1 = "Daniel";
-string saudacao1 = $"Ola, {nome}! Bem-vindo."
-Console.WriteLine(saudacao1)
+string saudacao1 = $"Ola, {nome}! Bem-vindo.";
+Console.WriteLine(saudacao1);
+//GetType(): identificar tipo de variavel
+int n1 = 5;
+Console.WriteLine("Tipo de variavel: " + n1.GetType());
+Console.WriteLine("Tipo de variavel: " + nome1.GetType());
