7759b6d6329129271e68fd5f15d8b03d26cc836f
Daniel-KKihara <kiharadaniel87@gmail.com>
2024-11-06 16:46:59 -0400

    adicionado else aninhado

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
 double n1 = 5.5;
 double n2 = 8.0;
 double n3 = 6.8;
@@ -7,4 +8,16 @@ if(media >= 7){
     Console.WriteLine($"\nMedia {media:F1}: Aprovado");
 } else {
     Console.WriteLine($"\nMedia {media:F1}: Recuperacao");
+} */
+
+double n4 = 5.5;
+double n5 = 8.0;
+double n6 = 6.9;
+double media2 = (n4+n5+n6)/3;
+if(media2 >= 7){
+    Console.WriteLine($"Media {media2:F1}: Aprovado");
+} else if(media2 < 7 && media2 >=5){
+    Console.WriteLine($"Media {media2:F1}: Recuperacao");
+} else {
+    Console.WriteLine($"Media {media2:F1}: Reprovado");
 }
