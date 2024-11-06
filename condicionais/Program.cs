8e3c63ed610009324a13478ffbc3a7467440117b
Daniel-KKihara <kiharadaniel87@gmail.com>
2024-11-06 15:59:41 -0400

    estruturas condicionais

---
+++ condicionais/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 6;
+int b = 5;
+if(a >= b){
+    Console.WriteLine("Verdadeiro");
+} else {
+    Console.WriteLine("Falso");
+}
+double n1 = 6;
+double n2 = 5;
+double n3 = 7;
+double media = (n1+n2+n3)/3;
+if(media >= 7){
+    Console.WriteLine("\nAluno aprovado com media: " + media);
+} else {
+    Console.WriteLine("\nAluno reprovado com media: " + media);
+}
