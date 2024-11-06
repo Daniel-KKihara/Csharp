3f13a4b363a2cc0f23190780b6d4894898b0280c
Daniel-KKihara <kiharadaniel87@gmail.com>
2024-11-06 16:09:06 -0400

    exemplo if else

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -1,17 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int a = 6;
-int b = 5;
-if(a >= b){
-    Console.WriteLine("Verdadeiro");
-} else {
-    Console.WriteLine("Falso");
-}
-double n1 = 6;
-double n2 = 5;
-double n3 = 7;
+double n1 = 5.5;
+double n2 = 8.0;
+double n3 = 6.8;
 double media = (n1+n2+n3)/3;
 if(media >= 7){
-    Console.WriteLine("\nAluno aprovado com media: " + media);
+    Console.WriteLine($"\nMedia {media:F1}: Aprovado");
 } else {
-    Console.WriteLine("\nAluno reprovado com media: " + media);
+    Console.WriteLine($"\nMedia {media:F1}: Reprovado");
 }
