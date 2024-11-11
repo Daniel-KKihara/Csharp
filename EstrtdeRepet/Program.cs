d6e66f10d17847aed8e68b424f8c17c157ae0fa3
Daniel-KKihara <kiharadaniel87@gmail.com>
2024-11-11 14:23:50 -0400

    exemplo de While

--- EstrtdeRepet/Program.cs
+++ EstrtdeRepet/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Random numeroAleat - new Random();
+//Estrutura de Repeticao While
+Random numeroAleat = new Random();
+int numalv = numeroAleat.Next(1, 101);
+int cont = 0;
+//Numero de condicao de parada do While
+Console.WriteLine($"Numero Alvo: {numalv}");
+while (cont != numalv) {
+    Console.WriteLine($"Contador: {cont}");
+    cont++;
+}
+Console.WriteLine($"\nO valor final do contador: {cont}\n");
